eeb6bfc7ee073184f84b48cce78452f34a8e351e
Krzysztof Cholewa E6540 <krzysztof.cholewa@gmail.com>
2021-11-11 22:57:07 +0100

    [GTB-6] adding image plugin and StaticImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
+import Layout from "../components/Layout/Layout"
+import { StaticImage } from "gatsby-plugin-image"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const Home = () => (
+  <Layout>
+    <main className="page">
+      <header className="hero">
+        <StaticImage src="../assets/images/main.jpg" alt="vegetables" placeholder="blurred" className="hero-img"
+                     layout="fullWidth" />
+        <div className="hero-container">
+          <div className="hero-text">
+            <h1>simply recipes</h1>
+          </div>
+        </div>
+      </header>
+    </main>
+  </Layout>
+)
+
+export default Home
