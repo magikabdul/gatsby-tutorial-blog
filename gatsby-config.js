c221c3e9eb6b61ebe9e2740b77346efb594c4815
Krzysztof Cholewa E6540 <krzysztof.cholewa@gmail.com>
2021-11-13 20:43:36 +0100

    [GTB-7] adding siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,20 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "Simple Recipes",
+    description: "Recipes site",
+    author: "@joshsmilga",
+    person: { name: "John", age: 32 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "john", age: 32 },
+      { name: "susan", age: 21 }
+    ]
+  },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-  ],
+    `gatsby-transformer-sharp`
+  ]
 }
